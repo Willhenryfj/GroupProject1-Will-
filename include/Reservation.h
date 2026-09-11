2d5e0e75baa8da516fda482bcaa5225fe8e249b8
Will <WilliamHenryfj@gmail.com>
2026-09-11 11:58:56 -0500

    Implement Reservation class with basic functions
    Added Reservation class with constructors, getters, setters, and a display method.

---
+++ include/Reservation.h
@@ -0,0 +1,68 @@
+#include <string>
+#include <iostream>
+
+class Reservation {
+    public:
+    //Constructors
+    Reservation();
+    Reservation(std::string reservationID, std::string studentID, std::string name, std:string resourceID, std:string date){};
+    
+    
+    //Getters and Setters
+    std::string getReservationID() 
+    {
+      
+    }
+    std::string getStudentID() 
+    {
+       
+    }
+    std::string getName() 
+    {
+       
+    }
+    std::string getResourceID() 
+    {
+       
+    }
+    std::string getDate() 
+    {
+       
+    }
+
+    void setResourceID(std::string reservationID) 
+    {
+        
+    }
+    void setStudentID(std::string studentID) 
+    {
+       
+    }
+    void setName(std::string name) 
+    {
+      
+    }
+    void setResourceID(std::string reservationID)
+    {
+
+    }
+    void setDate(std::string date)
+    {
+
+    }
+
+    //Display Resource Information to the console
+    void DisplayReservationInfo()
+    {
+          
+    }
+    private:
+    
+    //Types
+    std::string reservationID;
+    std::string studentID;
+    std::string name;
+    std::string reservationID;
+    std::string date; 
+
+};
